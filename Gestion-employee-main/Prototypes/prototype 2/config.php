eb7992d22cbe022aa676fb354c53978593fe63e2
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-05-05 11:59:00 +0000

    up

--- Gestion-employee-main/Prototypes/prototype 2/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $conn = mysqli_connect('localhost', 'soukayna000', 'test1234', 'employee');
-
-   // check connection
- if(!$conn){
-      echo 'Connection error: ' . mysqli_connect_error(); 
-  }
-?>
--- Gestion-employee-main/Prototypes/prototype 3 1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $conn = mysqli_connect('localhost', 'soukayna', '', 'prototype 3');
-
-   // check connection
- if(!$conn){
-      echo 'Connection error: ' . mysqli_connect_error(); 
-  }
-?>
